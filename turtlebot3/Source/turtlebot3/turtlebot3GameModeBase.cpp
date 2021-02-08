db26d401ef1e4daf1e5d2b12e87f88ead3afcdc5
Graeme Murray <graeme@chalice-corp.jp>
2021-02-08 16:15:21 +0900

    Initial project created from the editor

---
+++ turtlebot3/Source/turtlebot3/turtlebot3GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "turtlebot3GameModeBase.h"
+
---
+++ turtlebot3/Source/turtlebot3/turtlebot3GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "turtlebot3GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TURTLEBOT3_API Aturtlebot3GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
