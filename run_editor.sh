3b82e647093b49e4bb291f1b4d72774a8ea0fdab
Graeme Murray <graeme@chalice-corp.jp>
2021-02-10 16:38:33 +0900

    Enabling rclUE plugin as submodule

--- run_editor.sh
+++ run_editor.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export LD_LIBRARY_PATH="${PWD}/turtlebot3/Plugins/rclUE/Source/ThirdParty/ros2lib":"${PWD}/turtlebot3/Plugins/rclUE/Source/ThirdParty/ros2lib/std_msgs/lib"
+
 EDITOR_COMMAND="../UnrealEngine/Engine/Binaries/Linux/UE4Editor"
 
 (exec "$EDITOR_COMMAND" "${PWD}/turtlebot3/turtlebot3.uproject")
