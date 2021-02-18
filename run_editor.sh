84eb083c4e29c14c87bc767347a4b39e01400373
Graeme Murray <graeme@chalice-corp.jp>
2021-02-18 18:03:56 +0900

    Adding geometry message lib path

--- run_editor.sh
+++ run_editor.sh
@@ -4,6 +4,7 @@ export LD_LIBRARY_PATH=\
 "${PWD}/turtlebot3/Plugins/rclUE/Source/ThirdParty/ros2lib":\
 "${PWD}/turtlebot3/Plugins/rclUE/Source/ThirdParty/ros2lib/std_msgs/lib":\
 "${PWD}/turtlebot3/Plugins/rclUE/Source/ThirdParty/ros2lib/rosgraph_msgs/lib":\
+"${PWD}/turtlebot3/Plugins/rclUE/Source/ThirdParty/ros2lib/geometry_msgs/lib":\
 "${PWD}/turtlebot3/Plugins/rclUE/Source/ThirdParty/ros2lib/builtin_interfaces/lib":\
 "${PWD}/turtlebot3/Plugins/rclUE/Source/ThirdParty/ros2lib/ue4_interfaces/lib"
 
