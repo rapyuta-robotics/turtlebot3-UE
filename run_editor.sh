f282995579cfa500dc377ce0158e97800e589994
Graeme Murray <graeme@chalice-corp.jp>
2021-02-18 17:10:26 +0900

    Updating library path environment variable

--- run_editor.sh
+++ run_editor.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH="${PWD}/turtlebot3/Plugins/rclUE/Source/ThirdParty/ros2lib":"${PWD}/turtlebot3/Plugins/rclUE/Source/ThirdParty/ros2lib/std_msgs/lib"
+export LD_LIBRARY_PATH=\
+"${PWD}/turtlebot3/Plugins/rclUE/Source/ThirdParty/ros2lib":\
+"${PWD}/turtlebot3/Plugins/rclUE/Source/ThirdParty/ros2lib/std_msgs/lib":\
+"${PWD}/turtlebot3/Plugins/rclUE/Source/ThirdParty/ros2lib/rosgraph_msgs/lib":\
+"${PWD}/turtlebot3/Plugins/rclUE/Source/ThirdParty/ros2lib/builtin_interfaces/lib":\
+"${PWD}/turtlebot3/Plugins/rclUE/Source/ThirdParty/ros2lib/ue4_interfaces/lib"
 
 EDITOR_COMMAND="../UnrealEngine/Engine/Binaries/Linux/UE4Editor"
 
 (exec "$EDITOR_COMMAND" "${PWD}/turtlebot3/turtlebot3.uproject")
+
