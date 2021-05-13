23c25c817e31f700a6d4c390d61b939ccdfca2df
yu <yu@system76-pc.localdomain>
2021-05-13 10:29:51 +0900

    add pub/sub example in Cpp and Bp

---
+++ turtlebot3/Source/turtlebot3/pubsub/ROS2PublisherNode.cpp
@@ -0,0 +1,50 @@
+// Copyright (C) Rapyuta Robotics
+
+
+#include "ROS2PublisherNode.h"
+
+
+// Sets default values
+AROS2PublisherNode::AROS2PublisherNode()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AROS2PublisherNode::BeginPlay()
+{
+	Super::BeginPlay();
+	Init();
+
+	// Create String publisher
+	StringPublisher = NewObject<UROS2Publisher>(this, UROS2Publisher::StaticClass());
+	StringPublisher->RegisterComponent();
+	StringPublisher->TopicName = FString("test_topic");
+	StringPublisher->PublicationFrequencyHz = 1;
+	StringPublisher->MsgClass = UROS2StringMsg::StaticClass();
+	StringPublisher->UpdateDelegate.BindDynamic(this, &AROS2PublisherNode::MessageUpdate);
+	AddPublisher(StringPublisher);
+	StringPublisher->Init(UROS2QoS::DynamicBroadcaster);
+
+	// Msg data. You can change from editor.
+	// Fill default value if value is empty
+	Message =  Message.IsEmpty() ? FString("Hello from C++") : Message;
+
+}
+
+// MessageUpdate function is called with PublicationFrequencyHz
+void AROS2PublisherNode::MessageUpdate(UROS2GenericMsg *TopicMessage)
+{
+	UROS2StringMsg *StringMessage = Cast<UROS2StringMsg>(TopicMessage);
+	StringMessage->Update(Message);
+}
+
+// Called every frame
+void AROS2PublisherNode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
