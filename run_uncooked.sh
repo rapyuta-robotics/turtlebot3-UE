14d78dc9e00a8b3bb35baee1a213c14814f7cfd3
duc <ducanh.than@rapyuta-robotics.com>
2022-01-14 03:41:16 +0900

    Update submodules

--- run_uncooked.sh
+++ run_uncooked.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # Copyright 2020-2021 Rapyuta Robotics Co., Ltd.
-
 CURRENT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 TB3_UE_DIR=${1:-"${CURRENT_DIR}"}
 RCLUE_DIR="${TB3_UE_DIR}/Plugins/rclUE"
-
 source ${RCLUE_DIR}/Scripts/setup_ros2libs.sh ${RCLUE_DIR}
-
 UE_EDITOR=${2:-"$(dirname ${TB3_UE_DIR})/UnrealEngine/Engine/Binaries/Linux/UE4Editor"}
 (exec "$UE_EDITOR" "${TB3_UE_DIR}/turtlebot3.uproject" "-game")
