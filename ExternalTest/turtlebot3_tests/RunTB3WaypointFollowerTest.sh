3d695cfc9eaf3893bd73938e8b9ed772a1fb4d9f
duc <ducanh.than@rapyuta-robotics.com>
2022-02-16 20:12:16 +0900

    Make turtlebot3_tests a canonical ros2 package

--- ExternalTest/turtlebot3_tests/RunTB3WaypointFollowerTest.sh
+++ ExternalTest/turtlebot3_tests/RunTB3WaypointFollowerTest.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 mkdir maps
-ln ../../Content/Turtlebot3_benchmark.pgm maps/Turtlebot3_benchmark.pgm
-ln ../../Content/Turtlebot3_benchmark.yaml maps/Turtlebot3_benchmark.yaml
-launch_test src/test_waypoint_follower.py tb3_model:=$1
+if [ ! -f maps/Turtlebot3_benchmark.pgm ]; then
+    ln ../../Content/Turtlebot3_benchmark.pgm maps/Turtlebot3_benchmark.pgm
+fi
+if [ ! -f maps/Turtlebot3_benchmark.yaml ]; then
+    ln ../../Content/Turtlebot3_benchmark.yaml maps/Turtlebot3_benchmark.yaml
+fi
+
+launch_test turtlebot3_tests/test_waypoint_follower.py tb3_model:=$1
