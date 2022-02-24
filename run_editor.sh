451da20bec71d96e263a0021fe641ce8ec8d502c
duc <ducanh.than@rapyuta-robotics.com>
2022-02-24 21:12:01 +0900

    run bash script: Remove LD_LIBRARY_PATH configs

--- run_editor.sh
+++ run_editor.sh
@@ -3,24 +3,9 @@
 #source /home/cconti/Rapyuta/UnrealEngine/Engine/Build/BatchFiles/Linux/SetupEnvironment.sh -mono /home/cconti/Rapyuta/UnrealEngine/Engine/Build/BatchFiles/Linux;xbuild /property:Configuration=Development /verbosity:quiet /nologo /p:NoWarn=1591 /home/cconti/Rapyuta/UnrealEngine/Engine/Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj
 #mono /home/cconti/Rapyuta/UnrealEngine/Engine/Binaries/DotNET/UnrealBuildTool.exe -ModuleWithSuffix=turtlebot3,2890 turtlebot3Editor Linux Development -Project="/home/cconti/Rapyuta/turtlebot3/turtlebot3.uproject" "/home/cconti/Rapyuta/turtlebot3/turtlebot3.uproject"  -IgnoreJunk -progress
 
-export LD_LIBRARY_PATH=\
-"${PWD}/Plugins/rclUE/Source/ThirdParty/ros2lib":\
-"${PWD}/Plugins/rclUE/Source/ThirdParty/ros2lib/std_msgs/lib":\
-"${PWD}/Plugins/rclUE/Source/ThirdParty/ros2lib/geometry_msgs/lib":\
-"${PWD}/Plugins/rclUE/Source/ThirdParty/ros2lib/sensor_msgs/lib":\
-"${PWD}/Plugins/rclUE/Source/ThirdParty/ros2lib/rosgraph_msgs/lib":\
-"${PWD}/Plugins/rclUE/Source/ThirdParty/ros2lib/nav_msgs/lib":\
-"${PWD}/Plugins/rclUE/Source/ThirdParty/ros2lib/tf2_msgs/lib":\
-"${PWD}/Plugins/rclUE/Source/ThirdParty/ros2lib/builtin_interfaces/lib":\
-"${PWD}/Plugins/rclUE/Source/ThirdParty/ros2lib/ue4_interfaces/lib":\
-"${PWD}/Plugins/rclUE/Source/ThirdParty/ros2lib/ue_msgs/lib":\
-"${PWD}/Plugins/rclUE/Source/ThirdParty/ros2lib/rcutils/lib":\
-"${PWD}/Plugins/rclUE/Source/ThirdParty/ros2lib/rclc/lib":\
-"${PWD}/Plugins/rclUE/Source/ThirdParty/ros2lib/rcl/lib"
-
 #export ROS_MASTER_URI=http://localhost:11311
 
-unset ROS_DOMAIN_ID
+#unset ROS_DOMAIN_ID
 EDITOR_COMMAND="../UnrealEngine/Engine/Binaries/Linux/UE4Editor"
 
 #change default level
