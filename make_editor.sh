5b3bfd2de8348aaff32bc728b47a199083a84387
duc <ducanh.than@rapyuta-robotics.com>
2022-11-14 15:54:39 +0900

    Run scripts add DEFAULT_RATE DEFAULT_RTF ue5 make_editor.sh

--- make_editor.sh
+++ make_editor.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-make --file="./Makefile" turtlebot3Editor
+if [ -z "${UE5_DIR}" ]; then
+	printf "Please set UE5_DIR to path of UE5 UnrealEngine's parent folder\n"
+	exit 1
+fi
+
+PROJ_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+${UE5_DIR}/UnrealEngine/Engine/Binaries/DotNET/UnrealBuildTool/UnrealBuildTool Development Linux -Project="${PROJ_DIR}/turtlebot3.uproject" -TargetType=Editor #-Progress -NoEngineChanges -NoHotReloadFromIDE
--- run_uncooked_standalone.sh
+++ run_uncooked_standalone.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# Copyright 2020-2021 Rapyuta Robotics Co., Ltd.
+# Copyright 2020-2022 Rapyuta Robotics Co., Ltd.
+
+if [ -z "${UE5_DIR}" ]; then
+        printf "Please set UE5_DIR to path of UE5 UnrealEngine's parent folder\n"
+        exit 1
+fi
 
 CURRENT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 TB3_UE_DIR=${1:-"${CURRENT_DIR}"}
@@ -12,7 +17,11 @@ source ${TB3_UE_DIR}/fastdds_setup.sh
 
 #change default level, generating DefautlEngine.ini
 DEFAULT_LEVEL=${LEVEL_NAME:-"Turtlebot3_benchmark"}
+DEFAULT_RATE=${FIXED_FRAME_RATE:-"10.0"}
+DEFAULT_RTF=${TARGET_RTF:-"10.0"}
 sed -e 's/${LEVEL_NAME}/'${DEFAULT_LEVEL}'/g' Config/DefaultEngineBase.ini > Config/DefaultEngine.ini
+sed -i -e 's/${FIXED_FRAME_RATE}/'${DEFAULT_RATE}'/g' Config/DefaultEngine.ini
+sed -i -e 's/${TARGET_RTF}/'${DEFAULT_RTF}'/g' Config/DefaultEngine.ini
 
-UE_EDITOR=${2:-"$(dirname ${TB3_UE_DIR})/UnrealEngine/Engine/Binaries/Linux/UE4Editor"}
+UE_EDITOR="${UE5_DIR}/UnrealEngine/Engine/Binaries/Linux/UnrealEditor"
 (exec "$UE_EDITOR" "${TB3_UE_DIR}/turtlebot3.uproject" "-game")
