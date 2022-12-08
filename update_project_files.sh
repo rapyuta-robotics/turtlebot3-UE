cd664d71d0f1119a202fc98492a1eda98d47d89d
yuokamoto <yuokamoto1988@gmail.com>
2022-12-08 11:36:56 +0900

    update make it work with ue5(physics models are unstable (#48)

--- update_project_files.sh
+++ update_project_files.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-GENERATOR_COMMAND="../UnrealEngine/GenerateProjectFiles.sh"
+if [ -z "${UE5_DIR}" ]; then
+        printf "Please set UE5_DIR to path of UE5 UnrealEngine's folder\n"
+        exit 1
+fi
+
+GENERATOR_COMMAND="${UE5_DIR}/Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh"
+
 
 (exec "$GENERATOR_COMMAND" -project="${PWD}/turtlebot3.uproject" -game)
