ec7eb4e51a356944977f6afb21b23606648d8554
james.yookh <james.yookh@gmail.com>
2023-01-11 08:11:15 +0900

    fix: ue5 engine path
    * remove duplicated engine path

--- make_editor.sh
+++ make_editor.sh
@@ -6,4 +6,4 @@ if [ -z "${UE5_DIR}" ]; then
 fi
 
 PROJ_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-${UE5_DIR}/UnrealEngine/Engine/Binaries/DotNET/UnrealBuildTool/UnrealBuildTool Development Linux -Project="${PROJ_DIR}/turtlebot3.uproject" -TargetType=Editor #-Progress -NoEngineChanges -NoHotReloadFromIDE
+${UE5_DIR}/Engine/Binaries/DotNET/UnrealBuildTool/UnrealBuildTool Development Linux -Project="${PROJ_DIR}/turtlebot3.uproject" -TargetType=Editor #-Progress -NoEngineChanges -NoHotReloadFromIDE
